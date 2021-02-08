62169fdee15a317ba42086bd6f6e39466e1a152e
Shuruder <gabriel_brandini@hotmail.com>
2021-02-08 15:47:58 -0300

    Versão sobre Default Arguments e Lazy Evaluation

--- js/script.js
+++ js/script.js
@@ -1,71 +1,39 @@
-var obj = {
-    showContext: function showContext() {
-        //o this aqui auxilia no contexto de invocação
-        //o contexto de invocação executa a referencia baseada onde é invocada
-        //console.log(this)
-        //nesse caso também podemos invocar da seguinte forma
-        this.log('teste'); 
+// Default function Argument
+// DFA serve para situações onde caso esqueçamos de declarar um valor
+// temos um valor padrão para impedir erros garantindo o funcionamento
 
-        //outra forma de armazenar o contexto é criar uma variavel e armazenar o this
-        // var _that = this; //dessa forma bastava usar _that para invocar o contexto
-        // lado negativo é deixar um pouco poluido o código, e isso é resolvido com arrow function.
-
-
-        
-        /*setTimeout(function() {
-            this.log('after 1000ms');
-        }.bind(this), 1000); //bind ou apply fixa o contexto da função resolvendo possiveis problemas de contexto independente da onde foi invocada
-    },
-    log: function log(value) {
-        console.log(value);
-    }*/
-
-    // this = obj
-    // dessa forma não precisamos mais armazenar o this em variaveis e nem o bind
-    setTimeout(function() => {
-            this.log('after 1000ms');
-        }, 1000); //bind ou apply fixa o contexto da função resolvendo possiveis problemas de contexto independente da onde foi invocada
-    },
-    log: function log(value) {
-        console.log(value);
-    }
-};
-
-obj.showContext();
-
-var sumOld = function(a, b){
-    return a + b;
-};
-
-// Propriedade Hoishim? - capacidade de executar funções fora de ordem
-// obs: não funciona com arrow functions
-
-log('teste');
-
-function log(value){
-    console.log(value);
+/*
+function multiply (a, b){
+    // essa declaração faz com que b caso não declarado (false) seja igual a 1
+    b = b || 1;
+    return a * b;
 }
 
-// Arrow functions
-
-var createObj = () => ({ test: 123})
-
-console.log(createObj());
-
-// função construtora
-// obs: Não posso fazer o mesmo com arrow function
-
-function Car() {
-    this.foo = 'bar';
-}
-
-/* Erro em Arrow function - Car n é construtor
-
-var Car = () => {
-    this.foo = 'bar';
-};
-
+console.log(multiply(5));
 */
 
-console.log(new Car());
+/*
+// com o ES6 podemos definir valores padrão nos argumentos diretamente
+// Importante a ordem na argumentação, onde um argumento não pode ser reutiizado se nem foi declarado
+function multiply (a = 2, b = 1){
+    return a * b;
+}
 
+console.log(multiply(5));
+*/
+
+// lazy evaluation
+// quer dizer que a função só vai ativar quando for chamada
+// no caso em que o valor não seja declarado garantindo que seja acessada no momento certo
+function randomNumber() {
+    console.log('Generating a random number...');
+
+    return Math.random()*10;
+}
+
+function multiply(a, b = randomNumber()) {
+    return a * b;
+}
+
+console.log(multiply(5));
+console.log(multiply(5));
